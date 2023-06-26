624655499bc390083608451a3551fef7f4dec8f4
testname0531 <134988668+testname0531@users.noreply.github.com>
2023-06-26 16:23:23 +0800

    Update default.py

--- conf/default.py
+++ conf/default.py
@@ -22,8 +22,8 @@ from django.conf.global_settings import *  # noqa
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID = ''
-APP_TOKEN = ''
+APP_ID = 'test0992'
+APP_TOKEN = 'ab06149d-0e75-4616-8c13-0b3edbd37a08'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
 BK_PAAS_HOST = 'http://paas6.bktencent.com:80'
 
